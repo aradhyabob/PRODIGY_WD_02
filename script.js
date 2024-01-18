42ab2fbeb6070ebbab2b7f7abc781b50d857ccee
Aradhya <134131143+aradhyabob@users.noreply.github.com>
2024-01-18 10:12:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+var ms = 0, s = 0, m = 0, h = 0
+var timer;
+
+var display = document.querySelector(".timer-display")
+var laps = document.querySelector(".laps")
+
+function start(){
+  if(!timer){
+      timer = setInterval(run, 10)
+  }
+}
+
+function run(){
+  display.innerHTML = getTimer()
+  ms++              
+  if(ms == 100){
+      ms = 0
+      s++
+  }
+  if(s == 60){
+      s = 0
+      m++
+  }
+  if(m == 60){
+      m = 0
+      h++
+  }
+  
+  if(h == 13){
+      h = 1
+  } 
+}
+
+function pause(){
+  stopTimer()  
+}
+
+function stopTimer(){
+  clearInterval(timer)
+  timer = false 
+}
+
+function reset(){
+  stopTimer()
+  ms = 0
+  s = 0
+  m = 0
+  h = 0
+  display.innerHTML = getTimer()
+}
+
+function restart(){
+  if(timer){ 
+      reset()
+      start()
+  }
+  
+}
+
+// lap = taking screenshot of current time...
+function lap() {
+  if(timer) {   
+      var li = document.createElement("li")   
+      li.innerHTML = getTimer() 
+      laps.appendChild(li) 
+  }
+}
+
+function resetLap(){
+  laps.innerHTML = ""
+}
